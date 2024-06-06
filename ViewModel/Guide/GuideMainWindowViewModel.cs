389354f6533b8f85dab0676de494cfc5b5a40576
gari-tru <123237253+gari-tru@users.noreply.github.com>
2024-06-06 19:10:48 +0200

    a

--- ViewModel/Guide/GuideMainWindowViewModel.cs
+++ ViewModel/Guide/GuideMainWindowViewModel.cs
@@ -4,7 +4,7 @@ using BookingApp.View.Guide;
 
 namespace BookingApp.ViewModel.Guide
 {
-    class GuideMainWindowViewModel
+    class GuideMainWindowViewModel : ViewModelBase
     {
         private readonly int userId;
         public Frame NavigationService { get; set; }
@@ -13,6 +13,18 @@ namespace BookingApp.ViewModel.Guide
         public RelayCommand NavigateCreateTour { get; set; }
         public RelayCommand NavigateFinishedTours { get; set; }
         public RelayCommand NavigateTourRequests { get; set; }
+        public RelayCommand ToggleMenu { get; set; }
+
+        private bool _isMenuOpen = false;
+        public bool IsMenuOpen
+        {
+            get => _isMenuOpen;
+            set
+            {
+                _isMenuOpen = value;
+                OnPropertyChanged(nameof(IsMenuOpen));
+            }
+        }
 
         public GuideMainWindowViewModel(int userId, Frame navigationService)
         {
@@ -24,6 +36,12 @@ namespace BookingApp.ViewModel.Guide
             NavigateCreateTour = new RelayCommand(NavigateCreateTourExecute);
             NavigateFinishedTours = new RelayCommand(NavigateFinishedToursExecute);
             NavigateTourRequests = new RelayCommand(NavigateTourRequestsExecute);
+            ToggleMenu = new RelayCommand(ToggleMenuExecute);
+        }
+
+        private void ToggleMenuExecute(object parameter)
+        {
+            IsMenuOpen = !IsMenuOpen;
         }
 
         private void NavigateBackExecute(object parameter)
@@ -31,27 +49,32 @@ namespace BookingApp.ViewModel.Guide
             if (NavigationService.CanGoBack)
             {
                 NavigationService.GoBack();
+                IsMenuOpen = false;
             }
         }
 
         private void NavigateScheduledToursExecute(object parameter)
         {
             NavigationService.Navigate(new ScheduledToursPage(userId, NavigationService));
+            IsMenuOpen = false;
         }
 
         private void NavigateCreateTourExecute(object parameter)
         {
             NavigationService.Navigate(new CreateTourPage(null, userId, NavigationService));
+            IsMenuOpen = false;
         }
 
         private void NavigateFinishedToursExecute(object parameter)
         {
             NavigationService.Navigate(new FinishedToursPage(userId, NavigationService));
+            IsMenuOpen = false;
         }
 
         private void NavigateTourRequestsExecute(object parameter)
         {
             NavigationService.Navigate(new TourRequestsPage(userId, NavigationService));
+            IsMenuOpen = false;
         }
     }
 }
